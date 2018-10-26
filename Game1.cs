eb6e1d4fe06d4ce5ad1a0d9a90a5c6dad4ed14a4
Felix Ku <43415565+Felix-Ku@users.noreply.github.com>
2018-10-26 14:20:54 +0800

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    // Main game loop
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        // Represents the player 
+        Player player; /// Create instance of player class
+
+        // Initialize the player class
+        player = new Player(); /// Initialize new player for memory ready to store
+
+        // Load player graphic and set initial positions
+        protected override void LoadContent()
+        {
+            // Load the player resources 
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(Content.Load<Texture2D>("player"), playerPosition);
+        }
+
+        // For drawing anything inside game loop
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue)
+            
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+
+            // Stop drawing
+            spriteBatch.End();
+        }
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Player
+    {
+        // Animation representing the player
+        public Texture2D PlayerTexture;
+
+        // Position of the Player relative to the upper left side of the screen
+        public Vector2 Position;
+
+        // State of the player
+        public bool Active;
+
+        // Amount of hit points that player has
+        public int Health;
+
+        // Get the width of the player ship
+        public int Width
+        {
+            get { return PlayerTexture.Width; }
+        }
+
+        // Get the height of the player ship
+        public int Height
+        {
+            get { return PlayerTexture.Height; }
+        }
+
+        public void Initialize()
+        {
+            PlayerTexture = texture;
+
+            // Set the starting position of the player around the middle of the screen and to the back
+            Position = position;
+
+            // Set the player to be active
+            Active = true;
+
+            // Set the player health
+            Health = 100;
+        }
+
+        public void Update()
+        {
+        }
+
+        // The last step to draw the sprite is to queue Texture for rendering.We’ll do this by passing our texture to SpriteBatch within the Draw method.
+        public void Draw(SpriteBatch spriteBatch) //SpriteBatch is a class
+        {
+            spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
